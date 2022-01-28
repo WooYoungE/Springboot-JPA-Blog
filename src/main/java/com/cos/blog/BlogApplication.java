8cea14ae1fcc21483019db871611ae224964fe5a
WooYoung <wjsqhdrl4747@naver.com>
2022-01-29 00:04:09 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -0,0 +1,17 @@
+package com.cos.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
